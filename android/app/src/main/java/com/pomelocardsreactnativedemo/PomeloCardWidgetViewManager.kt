25d0fbb78bf0a798ecaf82ae5d0ba8ec0c0b446f
penafernando <fernando.pena@pomelo.la>
2023-03-23 16:44:30 -0300

    Connect `PomeloCardWidgetViewManager` to React Native project

--- android/app/src/main/java/com/pomelocardsreactnativedemo/PomeloCardWidgetViewManager.kt
+++ android/app/src/main/java/com/pomelocardsreactnativedemo/PomeloCardWidgetViewManager.kt
@@ -1,17 +1,19 @@
 package com.pomelocardsreactnativedemo
 
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.*
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.pomelo.cards.widgets.CardsResult
 import com.pomelo.cards.widgets.ui.card.image.PomeloCardView
-import com.pomelo.cards.widgets.ui.card.models.CardView
+import com.pomelocardsreactnativedemo.ui.ChangePinBottomSheet
 
 class PomeloCardWidgetViewManager(
     private val callerContext: ReactApplicationContext
@@ -20,31 +22,24 @@ class PomeloCardWidgetViewManager(
     override fun getName() = REACT_CLASS
 
     companion object {
-        const val REACT_CLASS = "RCTPomeloCardWidget"
+        const val REACT_CLASS = "PomeloCardWidgetView"
     }
 
-    // TODO: Jetpack Compose is not supported, I don't know how to return a Compose view here and keep the return type `PomeloCardView`.
-    // TODO: It must init PomeloCardView without any param, they must be set later on setupParams
-    override fun createViewInstance(context: ThemedReactContext) = PomeloCardView(
-        name = "Juan Perez",
-        lastFourDigits = "0334",
-        showCardData = false,
-        cardId = BuildConfig.CARD_ID,
-        onResultListener = { cardsResult, _ ->
-            when (cardsResult) {
-                CardsResult.NETWORK_ERROR -> {}
-                CardsResult.BIOMETRIC_ERROR -> {}
-                CardsResult.SUCCESS -> {}
-            }
-        },
-        onHideData = {}
-    )
-
-
+    override fun createViewInstance(context: ThemedReactContext) = PomeloCardView(context.currentActivity!!)
 
     @ReactProp(name = "setupParams")
-    fun setSetupParams(view: PomeloCardView, setupParams: Map<String, String>) {
-        // TODO: Setup view params here (name, lastFourDigits, cardId)
+    fun setSetupParams(view: PomeloCardView, setupParams: ReadableMap) {
+        val name = setupParams.getString("cardholderName") ?: ""
+        val lastFourDigits = setupParams.getString("lastFourCardDigits") ?: ""
+        val imageUrl = "file:///android_asset/card_image.png"
+
+        view.init(name, lastFourDigits, 0, imageUrl)
+    }
+
+    @ReactMethod
+    fun showSensitiveData(reactTag: Number, cardId: String, promise: Promise?) {
+        Log.d("PomeloCardsModule", "Launch change pin event called with cardId: $cardId")
+
     }
 
 }
